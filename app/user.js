dc68175959bbee60a7d317468171dc544cf8f9f2
Thomas Lind <tholind@UG.KTH.SE>
2018-03-21 15:33:06 +0100

    commit

--- app/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var User = (function () {
-    function User() {
-    }
-    return User;
-}());
-exports.User = User;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXNlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInVzZXIudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQTtJQUFBO0lBT0UsQ0FBQztJQUFELFdBQUM7QUFBRCxDQUFDLEFBUEgsSUFPRztBQVBVLG9CQUFJIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIFVzZXIge1xyXG4gICAgcHJpbWFyeV9pZDogc3RyaW5nO1xyXG4gICAgZnVsbF9uYW1lOiBzdHJpbmc7XHJcbiAgICBlbWFpbDogc3RyaW5nO1xyXG4gICAgc3RyZWV0YWRkcmVzczogc3RyaW5nO1xyXG4gICAgcG9zdGFsX2NvZGU6IHN0cmluZztcclxuICAgIGNpdHk6IHN0cmluZztcclxuICB9Il19
